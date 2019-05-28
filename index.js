075f0ef8e08350c4f424f3b38c3c46da4442224a
melissa-murphy <melissamurphy.ca@gmail.com>
2019-05-28 15:13:49 -0700

    post new user

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ server.get('/users', (req, res) => {
     });
 });
 
+// GET, USER BY ID
 server.get('/api/users/:id', (req, res) => {
   // get user by id
   database
@@ -38,24 +39,92 @@ server.get('/api/users/:id', (req, res) => {
     });
 });
 
+// DELETE, USER BY ID
 server.delete('/api/users/:id', (req, res) => {
   // delete user by id
   const id = req.params;
   database
     .remove(id)
     .then(res => {
-      if (res === 0) {
-        sendUserError(404, `User#${id} does not exist. Please try again.`, res);
+      if (res) {
+        res.status(200).json({ success: `User#${id} sucessfully removed.` });
+      } else {
+        res.status(404, `User#${id} does not exist. Please try again.`, res);
         return;
       }
-      res.json({ success: `User#${id} sucessfully queried.` });
     })
     .catch(error => {
-      sendUserError(500, `User#${id} does not exist. Please try again.`, res);
+      console.log(`-------------------Within catch, delete user(id)` + error);
+      sendUserError(
+        500,
+        `User#${id} does not exist and cannot be deleted. Please try again.`,
+        res
+      );
     });
 });
 
+// PUT, MODIFY USER BY ID
 server.put('/api/users/:id', (req, res) => {
   // updates user and returns modified doc/state
+  const { id } = req.params;
+  const { name, bio } = req.body;
+  if (!name || !body) {
+    sendUserError(400).json({
+      ErrorMessage: `Please provide the required name and bio`
+    });
+    return;
+  }
+  db.update(id, { name, bio }).then(response => {
+    if (response === 0) {
+      res.status(404).json({
+        ErrorMessage: `This user does not exist, please try again.`
+      });
+      return;
+    }
+    db.findById(id)
+      .then(user => {
+        if (user.length === 0) {
+          res
+            .status(404)
+            .json({ ErrorMessage: `User not found, please try again.` });
+          return;
+        }
+        res.status(201).json(user);
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({ ErrorMessage: `Server Error: Cannot find user.` });
+        return;
+      })
+      .catch(error => {
+        res.status(500).json({ ErrorMessage: `Internal Server Error` });
+      });
+  });
 });
+
+// POST, NEW USER
+server.post('/api/users', (req, res) => {
+  console.log(`----------------------------------requested body` + req.body);
+  const { name, bio, created_on, updated_on } = req.body;
+  if (!name || !bio) {
+    res.status(400).json({ ErrorMessage: `Name and Bio required.` });
+    return;
+  }
+  db.insert({
+    name,
+    bio,
+    created_on,
+    updated_on
+  })
+    .then(response => {
+      res.status(201).json(response);
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(400).json({ ErrorMessage: `Bad request, please try again.` });
+      return;
+    });
+});
+
 server.listen(8000, () => console.log('API Running on port 8000'));
